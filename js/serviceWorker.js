4477d637b7b506082f4b28615260ddd4fba9f8a1
qihong1983 <55377146@qq.com>
2021-01-06 09:01:36 +0800

    first commit

---
+++ js/serviceWorker.js
@@ -0,0 +1,47 @@
+const staticAssets = [
+	'./',
+	'./styles.css',
+	'./app.js',
+	'./fallback.json'
+];
+
+//
+
+self.addEventListener('install', async event => {
+	console.log(`install`);
+	const cache = await caches.open('news-static');
+	cache.addAll(staticAssets);
+});
+
+
+
+self.addEventListener('fetch', event => {
+	const req = event.request;
+	const url = new URL(req.url);
+	if (url.origin === location.origin) {
+		event.respondWith(cacheFirst(req));
+	} else {
+		event.respondWith(networkFirst(req));
+	}
+
+});
+
+async function cacheFirst(req) {
+	const cachedResponse = await caches.match(req);
+	return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+	const cache = await caches.open('news-dynamic');
+	try {
+		const res = await fetch(req)
+		cache.put(req, res.clone());
+		return res;
+	} catch (error) {
+		// return await cache.match(req);
+
+		// const await cache.match(req);
+		const cachedResponse = await cache.match(req);
+		return cachedResponse || await caches.match('./fallback.json');
+	}
+}
